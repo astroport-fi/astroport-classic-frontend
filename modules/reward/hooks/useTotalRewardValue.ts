77cc75f6b2901d9ab7d296aae65b589d61858455
Arthur <itey.arthur@gmail.com>
2021-12-29 00:47:21 +0800

    fix(reward): value should be in ASTRO

--- modules/reward/hooks/useTotalRewardValue.ts
+++ modules/reward/hooks/useTotalRewardValue.ts
@@ -1,20 +1,16 @@
 import { useMemo } from "react";
 
-import { useContracts } from "modules/common";
 import { usePhase1Rewards, usePhase2Rewards } from "modules/reward";
 import { useAirdropBalance } from "modules/airdrop";
-import { useTokenPriceInUst } from "modules/swap";
 
 export const useTotalRewardValue = () => {
-  const { astroToken } = useContracts();
-  const price = useTokenPriceInUst(astroToken);
   const phase1Rewards = usePhase1Rewards();
   const phase2Rewards = usePhase2Rewards();
   const airdropBalance = useAirdropBalance();
 
   return useMemo(() => {
-    return (airdropBalance + phase1Rewards + phase2Rewards) * price;
-  }, [airdropBalance, phase1Rewards, phase2Rewards, price]);
+    return airdropBalance + phase1Rewards + phase2Rewards;
+  }, [airdropBalance, phase1Rewards, phase2Rewards]);
 };
 
 export default useTotalRewardValue;
