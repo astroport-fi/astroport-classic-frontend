5dc893c51bf9f89e7716f82d063caf76999f63c5
Arthur <itey.arthur@gmail.com>
2021-12-28 15:45:05 +0800

    fix(airdrop): add real merkle roots

--- modules/airdrop/hooks/useAirdrop.ts
+++ modules/airdrop/hooks/useAirdrop.ts
@@ -2,29 +2,34 @@ import { useMemo } from "react";
 import { useQuery } from "react-query";
 
 export const useAirdrop = (address: string | undefined) => {
-  // const [value, setValue] = useState(null);
-
   const query = useQuery(["airdrop", 1], async () => {
     const res = await fetch("/airdrop/airdrop.json");
     return res.json();
   });
 
-  // const query2 = useQuery(["airdrop", 2], async () => {
-  //   const res = await fetch("/airdrop/airdrop2.json");
-  //   return res.json();
-  // });
+  const query2 = useQuery(["airdrop", 2], async () => {
+    const res = await fetch("/airdrop/airdrop2.json");
+    return res.json();
+  });
+
+  const query3 = useQuery(["airdrop", 3], async () => {
+    const res = await fetch("/airdrop/airdrop3.json");
+    return res.json();
+  });
 
   return useMemo(() => {
-    if (query.isLoading) {
+    if (query.isLoading || query2.isLoading || query3.isLoading) {
       return {
         isLoading: true,
         data: null,
       };
     }
 
-    const data = [...query.data].find((item) => {
-      return item.address === address;
-    });
+    const data = [...query.data, ...query2.data, ...query3.data].find(
+      (item) => {
+        return item.address === address;
+      }
+    );
 
     return {
       isLoading: false,
