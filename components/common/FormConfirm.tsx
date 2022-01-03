e41d74db4f8556c122339e93a51aebab67f00e44
Swann Polydor <swann.polydor@epitech.eu>
2022-01-03 09:46:20 +0200

    Give formConfirm a maxWidth instead of width (#168)

--- components/common/FormConfirm.tsx
+++ components/common/FormConfirm.tsx
@@ -44,7 +44,7 @@ const FormConfirm: FC<Props> = ({
     <MotionBox
       initial={{ opacity: 0, scale: 0.8 }}
       animate={{ opacity: 1, scale: 1 }}
-      w="470px"
+      maxW="470px"
       m="0 auto"
       mt="10"
     >
