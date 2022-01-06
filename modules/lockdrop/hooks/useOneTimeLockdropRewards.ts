1b29d34e2eaf682d847b8343eda59b81d0b1603e
Arthur <itey.arthur@gmail.com>
2022-01-06 19:17:33 +0800

    fix(price): better price estimation

--- modules/lockdrop/hooks/useOneTimeLockdropRewards.ts
+++ modules/lockdrop/hooks/useOneTimeLockdropRewards.ts
@@ -1,17 +1,17 @@
 import { useMemo } from "react";
+import { num } from "@arthuryeti/terra";
 
 import { usePhase1Rewards, usePhase2Rewards } from "modules/reward";
 import { useAirdropBalance } from "modules/airdrop";
 import { useContracts } from "modules/common";
-import { useTokenPriceInUst } from "modules/swap";
-import { num } from "@arthuryeti/terra";
+import { useTokenPriceInUstWithSimulate } from "modules/swap";
 
 export const useOneTimeLockdropRewards = () => {
   const { astroToken } = useContracts();
   const phase1Rewards = usePhase1Rewards();
   const phase2Rewards = usePhase2Rewards();
   const airdropBalance = useAirdropBalance();
-  const astroPrice = useTokenPriceInUst(astroToken);
+  const astroPrice = useTokenPriceInUstWithSimulate(astroToken);
 
   return useMemo(() => {
     return num(airdropBalance + phase1Rewards + phase2Rewards)
