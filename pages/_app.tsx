486459c59e2b9cb649652f031a136e5dc8ec54de
Andre Sardo <afsardo@gmail.com>
2022-01-14 03:57:26 +0000

    fix(viewport): avoid browsers zoom in mobile devices with meta viewport (#215)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -45,6 +45,7 @@ const MyApp = ({
   const main = (
     <>
       <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
         <link rel="shortcut icon" href="/favicon.png" />
       </Head>
       <QueryClientProvider client={queryClient}>
