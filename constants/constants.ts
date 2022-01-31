0b95e8e094baddcc14bf26d18eb9c88b25cf3fc0
Andre Sardo <afsardo@gmail.com>
2022-01-31 23:33:34 +0000

    fix: remove full stop in rewards notice

--- constants/constants.ts
+++ constants/constants.ts
@@ -25,7 +25,7 @@ export const SHARE_TOKEN = "terra1l5nrdu9550yjpt2gltv5szm9qzltztmtlld445";
 export const COMMON_TOKENS = ["uusd", "uluna"];
 
 export const REWARDS_NOTICE =
-  "The APY/APR are calculated using token prices denominated in UST. Prices are fetched either from Astroport pools or from Coingecko. Also, the APY/APR are 365 day projections based on each pool's performance over the last 24h. See Astroport Disclaimers & Disclosures for more details.";
+  "The APY/APR are calculated using token prices denominated in UST. Prices are fetched either from Astroport pools or from Coingecko. Also, the APY/APR are 365 day projections based on each pool's performance over the last 24h. See Astroport Disclaimers & Disclosures for more details";
 
 /* deployment env */
 export const DEPLOYMENT_PREVIEW =
