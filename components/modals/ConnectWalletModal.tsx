14e0659faac2568616b68ca7f9f6e8bafbf2ffa1
DW <dannywitters@gmail.com>
2022-02-02 13:51:07 +0100

    feat: wallet install options [ASTRO-1253]

--- components/modals/ConnectWalletModal.tsx
+++ components/modals/ConnectWalletModal.tsx
@@ -3,15 +3,47 @@ import { useWallet, ConnectType } from "@terra-money/wallet-provider";
 import { Text, HStack, Flex, chakra, Image } from "@chakra-ui/react";
 import Modal from "components/modals/Modal";
 import TerraExtensionIcon from "components/icons/TerraExtensionIcon";
-import TerraMobileIcon from "components/icons/TerraMobileIcon";
 
 type Props = {
   isOpen: boolean;
   onClose: () => void;
 };
 
+type WalletOptions = {
+  identifier: string;
+  name: string;
+  icon: string;
+  isInstalled?: boolean;
+  walletAction: () => void;
+};
+
 const ConnectWalletModal: FC<Props> = ({ isOpen, onClose }) => {
-  const { connect, availableConnections } = useWallet();
+  const { connect, availableInstallations, availableConnections } = useWallet();
+
+  const wallets: WalletOptions[] = [
+    ...availableConnections
+      .filter(({ type }) => type !== ConnectType.READONLY)
+      .map(({ type, icon, name, identifier }) => ({
+        identifier,
+        name,
+        icon,
+        isInstalled: true,
+        walletAction: () => {
+          connect(type, identifier);
+        },
+      })),
+    ...availableInstallations
+      .filter(({ type }) => type !== ConnectType.READONLY)
+      .map(({ icon, name, url, identifier }) => ({
+        identifier,
+        name: "Install " + name,
+        icon,
+        isInstalled: true,
+        walletAction: () => {
+          window.open(url, "_blank");
+        },
+      })),
+  ];
 
   return (
     <Modal isOpen={isOpen} onClose={onClose} title="Connect to a wallet">
@@ -21,59 +53,36 @@ const ConnectWalletModal: FC<Props> = ({ isOpen, onClose }) => {
         align="center"
         textAlign="center"
       >
-        {availableConnections
-          .filter(({ type }) => type === ConnectType.EXTENSION)
-          .map(({ identifier, name, icon }) => (
-            <chakra.button
-              key={identifier}
-              transition="0.2s all"
-              p="6"
-              borderRadius="xl"
-              bg="brand.purple"
-              color="white"
-              width="100%"
-              mb="4"
-              _hover={{
-                bg: "white",
-                color: "brand.dark",
-              }}
-              d={["none", "block"]}
-              onClick={() => {
-                onClose();
-                connect(ConnectType.EXTENSION, identifier);
-              }}
-            >
-              <HStack justify="space-between">
-                <Text>{name} Extension</Text>
-                {identifier === "station" ? (
-                  <TerraExtensionIcon />
-                ) : (
-                  <Image src={icon} htmlWidth="24" alt="" />
-                )}
-              </HStack>
-            </chakra.button>
-          ))}
-        <chakra.button
-          transition="0.2s all"
-          p="6"
-          borderRadius="xl"
-          bg="brand.purple"
-          color="white"
-          width="100%"
-          _hover={{
-            bg: "white",
-            color: "brand.dark",
-          }}
-          onClick={() => {
-            onClose();
-            connect(ConnectType.WALLETCONNECT);
-          }}
-        >
-          <HStack justify="space-between">
-            <Text>Terra Station Mobile</Text>
-            <TerraMobileIcon width="1.5rem" height="1.5rem" />
-          </HStack>
-        </chakra.button>
+        {Object.entries(wallets).map(([key, values]) => (
+          <chakra.button
+            key={key}
+            transition="0.2s all"
+            p="6"
+            borderRadius="xl"
+            bg="brand.purple"
+            color="white"
+            width="100%"
+            mb="4"
+            _hover={{
+              bg: "white",
+              color: "brand.dark",
+            }}
+            d={["none", "block"]}
+            onClick={() => {
+              onClose();
+              values.walletAction();
+            }}
+          >
+            <HStack justify="space-between">
+              <Text>{values.name} Extension</Text>
+              {values.identifier === "station" ? (
+                <TerraExtensionIcon />
+              ) : (
+                <Image src={values.icon} htmlWidth="24" alt="" />
+              )}
+            </HStack>
+          </chakra.button>
+        ))}
       </Flex>
     </Modal>
   );
