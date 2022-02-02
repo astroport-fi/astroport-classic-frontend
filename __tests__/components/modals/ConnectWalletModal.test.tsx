3f369f41ce12be9141ef781e4830302eb53b46e3
Jim Ryan <jim@room118solutions.com>
2022-02-02 18:20:27 -0500

    test: update ConnectWalletModal test for new install options
    This was broken/failing after 14e0659.

--- __tests__/components/modals/ConnectWalletModal.test.tsx
+++ __tests__/components/modals/ConnectWalletModal.test.tsx
@@ -1,6 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import ConnectWalletModal from "components/modals/ConnectWalletModal";
 import { ConnectType, useWallet, Wallet } from "@terra-money/wallet-provider";
+import userEvent from "@testing-library/user-event";
 
 jest.mock("@terra-money/wallet-provider", () => {
   const original = jest.requireActual("@terra-money/wallet-provider");
@@ -15,12 +16,13 @@ const useWalletMock = (overrides) => {
   return (): Wallet => ({
     ...jest.requireActual("@terra-money/wallet-provider").useWallet,
     availableConnections: [],
+    availableInstallations: [],
     ...overrides,
   });
 };
 
 describe("ConnectWalletModal", () => {
-  it("renders a button for each extension and mobile", () => {
+  beforeEach(() => {
     const availableConnections = [
       {
         type: ConnectType.EXTENSION,
@@ -40,32 +42,78 @@ describe("ConnectWalletModal", () => {
         name: "Foo Wallet",
         icon: "http://example.com/foo-wallet.svg",
       },
-
-      // Non-extension connections are ignored by component
       {
         type: ConnectType.WALLETCONNECT,
         name: "Wallet Connect",
         icon: "http://example.com/wallet-connect.svg",
       },
+
+      // Read-only wallets are ignored by the component
+      {
+        type: ConnectType.READONLY,
+        identifier: "readonly-wallet",
+        name: "Readonly Wallet",
+        icon: "http://example.com/readonly-wallet.svg",
+      },
+    ];
+
+    const availableInstallations = [
+      {
+        type: ConnectType.EXTENSION,
+        identifier: "xdefi-wallet",
+        name: "XDEFI Wallet",
+        icon: "http://example.com/xdefi-wallet.svg",
+        url: "http://example.com/install-xdefi-wallet",
+      },
     ];
 
     (useWallet as jest.Mock<Wallet>).mockImplementation(
-      useWalletMock({ availableConnections })
+      useWalletMock({ availableConnections, availableInstallations })
     );
+  });
+
+  it("renders a button for each available and installable extension", () => {
+    render(<ConnectWalletModal isOpen={true} onClose={() => {}} />);
+
+    // available connections
+    expect(
+      screen.getByRole("button", { name: "Terra Station" })
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole("button", { name: "Leap Wallet" })
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole("button", { name: "Foo Wallet" })
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole("button", { name: "Wallet Connect" })
+    ).toBeInTheDocument();
+
+    // installable connections
+    expect(
+      screen.getByRole("button", { name: "Install XDEFI Wallet" })
+    ).toBeInTheDocument();
+
+    // available read-only connections
+    expect(
+      screen.queryByRole("button", { name: "Readonly Wallet" })
+    ).not.toBeInTheDocument();
+  });
+
+  it("opens install URL when installable connection button is clicked", () => {
+    const windowSpy = jest.spyOn(window, "open").mockImplementation(jest.fn());
 
     render(<ConnectWalletModal isOpen={true} onClose={() => {}} />);
 
-    expect(
-      screen.getByRole("button", { name: "Terra Station Extension" })
-    ).toBeInTheDocument();
-    expect(
-      screen.getByRole("button", { name: "Leap Wallet Extension" })
-    ).toBeInTheDocument();
-    expect(
-      screen.getByRole("button", { name: "Foo Wallet Extension" })
-    ).toBeInTheDocument();
-    expect(
-      screen.getByRole("button", { name: "Terra Station Mobile" })
-    ).toBeInTheDocument();
+    userEvent.click(
+      screen.getByRole("button", { name: "Install XDEFI Wallet" })
+    );
+
+    expect(windowSpy).toHaveBeenCalledWith(
+      "http://example.com/install-xdefi-wallet",
+      "_blank"
+    );
+
+    windowSpy.mockRestore();
   });
 });
