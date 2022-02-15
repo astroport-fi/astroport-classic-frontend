db9113113b2acfa560ed5202960124ddbb09e1d5
Jim Ryan <jim@room118solutions.com>
2022-02-15 16:54:59 -0500

    ASTRO-1025 Add user-friendly error messages for Cosmos errors (#259)
    * Map transaction (SDK) errors to user-friendly messages
    
    * Map wasm errors

---
+++ __tests__/components/notifications/FailedNotification.test.tsx
@@ -0,0 +1,107 @@
+import FailedNotification from "components/notifications/FailedNotification";
+import { render, screen } from "@testing-library/react";
+import React from "react";
+
+describe("FailedNotification", () => {
+  it("displays friendly message for transaction parse errors (SDK 2)", () => {
+    const txInfo: any = {
+      code: 2,
+      codespace: "sdk",
+      raw_log: "raw error message",
+    };
+
+    render(<FailedNotification txInfo={txInfo} />);
+
+    expect(
+      screen.getByText(
+        "We're sorry, we were unable to parse your transaction. Please try again."
+      )
+    ).toBeInTheDocument();
+  });
+
+  it("displays friendly message for mempool full errors (SDK 20)", () => {
+    const txInfo: any = {
+      codespace: "sdk",
+      code: 20,
+      raw_log: "raw error message",
+    };
+
+    render(<FailedNotification txInfo={txInfo} />);
+
+    expect(
+      screen.getByText(
+        "Sorry, the mempool is full and cannot accept any more transactions. Please try again later."
+      )
+    ).toBeInTheDocument();
+  });
+
+  it("displays raw_log when codespace is missing from txinfo", () => {
+    const txInfo: any = {
+      code: 2,
+      raw_log: "raw error message",
+    };
+
+    render(<FailedNotification txInfo={txInfo} />);
+
+    expect(screen.getByText("raw error message")).toBeInTheDocument();
+  });
+
+  it("displays raw_log when code is missing from txinfo", () => {
+    const txInfo: any = {
+      codespace: "sdk",
+      raw_log: "raw error message",
+    };
+
+    render(<FailedNotification txInfo={txInfo} />);
+
+    expect(screen.getByText("raw error message")).toBeInTheDocument();
+  });
+
+  it("displays raw_log when codespace does not have an error mapping", () => {
+    const txInfo: any = {
+      codespace: "oracle",
+      code: 2,
+      raw_log: "raw error message",
+    };
+
+    render(<FailedNotification txInfo={txInfo} />);
+
+    expect(screen.getByText("raw error message")).toBeInTheDocument();
+  });
+
+  it("displays raw_log when codespace has mappings, but one is missing for the code", () => {
+    const txInfo: any = {
+      codespace: "sdk",
+      code: 42,
+      raw_log: "raw error message",
+    };
+
+    render(<FailedNotification txInfo={txInfo} />);
+
+    expect(screen.getByText("raw error message")).toBeInTheDocument();
+  });
+
+  it("displays raw_log when codespace (sdk) and code has a mapping, but there is no friendly error message for it", () => {
+    const txInfo: any = {
+      codespace: "sdk",
+      code: 36, // CosmosError.Conflict
+      raw_log: "raw error message",
+    };
+
+    render(<FailedNotification txInfo={txInfo} />);
+
+    expect(screen.getByText("raw error message")).toBeInTheDocument();
+  });
+
+  it("displays raw_log when codespace (wasm) and code has a mapping, but there is no friendly error message for it", () => {
+    const txInfo: any = {
+      codespace: "wasm",
+      code: 4, // WasmError.ExecuteFailed
+      raw_log: "contract execute failed",
+    };
+
+    render(<FailedNotification txInfo={txInfo} />);
+
+    expect(screen.getByText("contract execute failed")).toBeInTheDocument();
+  });
+});
--- components/notifications/FailedNotification.tsx
+++ components/notifications/FailedNotification.tsx
@@ -1,15 +1,107 @@
 import React, { FC } from "react";
 import { TxInfo } from "@terra-money/terra.js";
 import { Text } from "@chakra-ui/react";
+import { TERRA_ERROR_MAP, CosmosError } from "constants/terra";
 
 type Props = {
   txInfo: TxInfo;
 };
 
-const FailedNotification: FC<Props> = ({ txInfo }) => {
-  const { raw_log } = txInfo;
+const friendlyMessages = {
+  [CosmosError.TxParseError]:
+    "We're sorry, we were unable to parse your transaction. Please try again.",
+  [CosmosError.InvalidSequence]:
+    "We're sorry, your signature sequence was invalid. Please try again.",
+  [CosmosError.Unauthorized]:
+    "We're sorry, you don't have the authorization to carry out this request. Verify your permissions and try again.",
+  [CosmosError.InsufficientFunds]:
+    "We're sorry, you don't have enough funds to complete this request. Please try again when you have more funds available.",
+  [CosmosError.UnknownRequest]:
+    "We're sorry, we couldn't identify the request you made.",
+  [CosmosError.InvalidAddress]:
+    "We're sorry, that address is not valid. Please try again with a valid address.",
+  [CosmosError.InvalidPubkey]:
+    "We're sorry, that public key is not valid. Please try again with a valid public key.",
+  [CosmosError.UnknownAddress]:
+    "We're sorry, we couldn't find the address you specified. Please try again.",
+  [CosmosError.InvalidCoins]: "Sorry, the coins you specified are not valid.",
+  [CosmosError.OutOfGas]:
+    "Sorry, your request ran out of gas. Please try again with a higher gas limit.",
+  [CosmosError.MemoTooLarge]:
+    "Sorry, the amount of text you included in your memo was too large. Please try again with a smaller amount of text.",
+  [CosmosError.InsufficientFee]:
+    "Sorry, the fee you specified was not enough to cover the cost of this transaction. Please try again with a higher fee.",
+  [CosmosError.MaxSignaturesExceeded]:
+    "Sorry, the maximum number of signatures has been reached. Please try again with fewer signatures.",
+  [CosmosError.NoSignatures]:
+    "Sorry, we didn't receive any signatures for this transaction. Please try again with at least one signature.",
+  [CosmosError.FailedToMarshalJSON]:
+    "Sorry, Cosmos was unable to encode your JSON bytes. Please try again with valid JSON data.",
+  [CosmosError.FailedToUnmarshalJSON]:
+    "Sorry, Cosmos was not able to decode your JSON bytes. Please try again with valid JSON data.",
+  [CosmosError.InvalidRequest]:
+    "Sorry, that request was not valid. Please try again with a valid request.",
+  [CosmosError.TxAlreadyInMempool]:
+    "Sorry, that transaction was already being processed in the mempool. Please try again later.",
+  [CosmosError.MempoolFull]:
+    "Sorry, the mempool is full and cannot accept any more transactions. Please try again later.",
+  [CosmosError.TxTooLarge]:
+    "Sorry, the transaction size is too large. Please try again with a smaller transaction.",
+  [CosmosError.KeyNotFound]:
+    "Sorry, that key was not found in our system. Please try again with a different key.",
+  [CosmosError.InvalidAccountPassword]:
+    "Sorry, the key password you entered is not valid. Please try again with a valid key password.",
 
-  return <Text textStyle={["small", "medium"]}>{raw_log}</Text>;
+  // TODO
+  // [CosmosError.TxSignerMismatch]: "",
+  // [CosmosError.InvalidGasAdjustment]: "",
+  // [CosmosError.InvalidHeight]: "",
+  // [CosmosError.InvalidVersion]: "",
+  // [CosmosError.InvalidChainID]: "",
+  // [CosmosError.InvalidType]: "",
+  // [CosmosError.TxTimeoutHeight]: "",
+  // [CosmosError.UnknownExtensionOptions]: "",
+  // [CosmosError.IncorrectAccountSequence]: "",
+  // [CosmosError.ProtobufPackingFailed]: "",
+  // [CosmosError.ProtobufUnpackingFailed]: "",
+  // [CosmosError.InternalLogicError]: "",
+  // [CosmosError.Conflict]: "",
+  // [CosmosError.FeatureNotSupported]: "",
+  // [CosmosError.NotFound]: "",
+  // [CosmosError.InternalIOError]: "",
+  // [CosmosError.ErrorInAppToml]: "",
+
+  // [WasmError.ContractAccountExists]: "",
+  // [WasmError.InstantiateFailed]: "",
+  // [WasmError.ExecuteFailed]: "",
+  // [WasmError.InsufficientGas]: "",
+  // [WasmError.InvalidGenesis]: "",
+  // [WasmError.NotFound]: "",
+  // [WasmError.InvalidMsg]: "",
+  // [WasmError.NoRegisteredQuerier]: "",
+  // [WasmError.NoRegisteredParser]: "",
+  // [WasmError.MigrationFailed]: "",
+  // [WasmError.NotMigratable]: "",
+  // [WasmError.StoreCodeFailed]: "",
+  // [WasmError.ContractQueryFailed]: "",
+  // [WasmError.ExceedMaxContractSize]: "",
+  // [WasmError.ExceedMaxContractMsgSize]: "",
+  // [WasmError.ExceedMaxContractDataSize]: "",
+  // [WasmError.ReplyFailed]: "",
+  // [WasmError.ExceedMaxQueryDepth]: "",
+};
+
+const FailedNotification: FC<Props> = ({ txInfo }) => {
+  const { codespace, code, raw_log } = txInfo;
+
+  let message;
+
+  if (codespace && code) {
+    const terraError = TERRA_ERROR_MAP[codespace]?.[code];
+    message = friendlyMessages[terraError];
+  }
+
+  return <Text textStyle={["small", "medium"]}>{message ?? raw_log}</Text>;
 };
 
 export default FailedNotification;
