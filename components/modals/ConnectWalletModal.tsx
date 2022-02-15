60aac5da2d400c81999c46759ccd66018bb140da
DW <dannywitters@gmail.com>
2022-02-15 22:04:31 +0000

    fix: wallets on mobile devices

--- components/modals/ConnectWalletModal.tsx
+++ components/modals/ConnectWalletModal.tsx
@@ -11,7 +11,8 @@ type Props = {
 };
 
 type WalletOptions = {
-  identifier: string;
+  type: string;
+  identifier?: string;
   name: string;
   icon: string;
   isInstalled?: boolean;
@@ -19,7 +20,7 @@ type WalletOptions = {
 };
 
 const getIconImg = (identifier: string, icon: string) => {
-  if (identifier === "terra") {
+  if (identifier === "station") {
     return <TerraExtensionIcon />;
   } else if (identifier == "xdefi-wallet") {
     return <XDefiIcon />;
@@ -35,6 +36,7 @@ const ConnectWalletModal: FC<Props> = ({ isOpen, onClose }) => {
     ...availableConnections
       .filter(({ type }) => type !== ConnectType.READONLY)
       .map(({ type, icon, name, identifier }) => ({
+        type,
         identifier,
         name,
         icon,
@@ -45,7 +47,8 @@ const ConnectWalletModal: FC<Props> = ({ isOpen, onClose }) => {
       })),
     ...availableInstallations
       .filter(({ type }) => type !== ConnectType.READONLY)
-      .map(({ icon, name, url, identifier }) => ({
+      .map(({ type, icon, name, url, identifier }) => ({
+        type,
         identifier,
         name: "Install " + name,
         icon,
@@ -56,6 +59,31 @@ const ConnectWalletModal: FC<Props> = ({ isOpen, onClose }) => {
       })),
   ];
 
+  // on mobile, will need to add terra-station and wallet-connect
+  // as above code will return empty array
+  if (!wallets.some((key) => key.type === "WALLETCONNECT")) {
+    wallets.push({
+      type: ConnectType.WALLETCONNECT,
+      name: "Wallet Connect",
+      icon: "https://assets.terra.money/icon/wallet-provider/walletconnect.svg",
+      walletAction: () => {
+        connect(ConnectType.WALLETCONNECT);
+      },
+    });
+  }
+
+  if (!wallets.some((key) => key.identifier === "station")) {
+    wallets.push({
+      type: ConnectType.EXTENSION,
+      identifier: "station",
+      name: "Terra Station Wallet",
+      icon: "https://assets.terra.money/icon/station-extension/icon.png",
+      walletAction: () => {
+        connect(ConnectType.EXTENSION, "station");
+      },
+    });
+  }
+
   return (
     <Modal isOpen={isOpen} onClose={onClose} title="Connect to a wallet">
       <Flex
@@ -64,32 +92,33 @@ const ConnectWalletModal: FC<Props> = ({ isOpen, onClose }) => {
         align="center"
         textAlign="center"
       >
-        {Object.entries(wallets).map(([key, values]) => (
-          <chakra.button
-            key={key}
-            transition="0.2s all"
-            p="6"
-            borderRadius="xl"
-            bg="brand.purple"
-            color="white"
-            width="100%"
-            mb="4"
-            _hover={{
-              bg: "white",
-              color: "brand.dark",
-            }}
-            d={["none", "block"]}
-            onClick={() => {
-              onClose();
-              values.walletAction();
-            }}
-          >
-            <HStack justify="space-between">
-              <Text>{values.name}</Text>
-              {getIconImg(values.identifier, values.icon)}
-            </HStack>
-          </chakra.button>
-        ))}
+        {wallets.map((wallet, index) => {
+          return (
+            <chakra.button
+              key={index}
+              transition="0.2s all"
+              p="6"
+              borderRadius="xl"
+              bg="brand.purple"
+              color="white"
+              width="100%"
+              mb="4"
+              _hover={{
+                bg: "white",
+                color: "brand.dark",
+              }}
+              onClick={() => {
+                onClose();
+                wallet.walletAction();
+              }}
+            >
+              <HStack justify="space-between">
+                <Text>{wallet.name}</Text>
+                {getIconImg(wallet.identifier, wallet.icon)}
+              </HStack>
+            </chakra.button>
+          );
+        })}
       </Flex>
     </Modal>
   );
