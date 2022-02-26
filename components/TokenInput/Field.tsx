22a4e1b0fdb4f4687af57b91d15888b28707a1f0
Jim Ryan <jim@room118solutions.com>
2022-02-26 15:46:53 -0500

    fix: error caused by passing ref to TokenInput Field
    The TokenInput Field component is often passed
    all field props from a react-hook-form Controller's
    render function. This includes a ref. But refs cannot
    be passed directly to function components.
    
    This returns a forwardRef component instead, which
    accepts a ref, and simply ignores it for now.

--- components/TokenInput/Field.tsx
+++ components/TokenInput/Field.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from "react";
-import { Box } from "@chakra-ui/react";
+import { Box, forwardRef } from "@chakra-ui/react";
 
 import { Select, Single, SingleLP, SelectLP } from "components/TokenInput";
 import { useAstroswap } from "modules/common";
@@ -14,47 +14,44 @@ type Props = {
   value: string;
 };
 
-const Field: FC<Props> = ({
-  hideToken,
-  value,
-  onChange,
-  isSingle,
-  hidePrice,
-  isLpToken,
-  tokens,
-}) => {
-  const { tokens: allTokens } = useAstroswap();
-  const currentTokens = tokens ?? Object.keys(allTokens);
+const Field: FC<Props> = forwardRef(
+  (
+    { hideToken, value, onChange, isSingle, hidePrice, isLpToken, tokens },
+    ref
+  ) => {
+    const { tokens: allTokens } = useAstroswap();
+    const currentTokens = tokens ?? Object.keys(allTokens);
 
-  const handleClick = (asset: string) => {
-    onChange(asset);
-  };
+    const handleClick = (asset: string) => {
+      onChange(asset);
+    };
 
-  const renderSingle = () => {
-    if (isLpToken) {
-      return <SingleLP asset={value} />;
-    }
+    const renderSingle = () => {
+      if (isLpToken) {
+        return <SingleLP asset={value} />;
+      }
 
-    return <Single asset={value} hidePrice={hidePrice} />;
-  };
+      return <Single asset={value} hidePrice={hidePrice} />;
+    };
 
-  const renderSelect = () => {
-    if (isLpToken) {
-      return <SelectLP value={value} onClick={handleClick} />;
-    }
+    const renderSelect = () => {
+      if (isLpToken) {
+        return <SelectLP value={value} onClick={handleClick} />;
+      }
 
-    return (
-      <Select
-        value={value}
-        tokens={currentTokens}
-        hideToken={hideToken}
-        hidePrice={hidePrice}
-        onClick={handleClick}
-      />
-    );
-  };
+      return (
+        <Select
+          value={value}
+          tokens={currentTokens}
+          hideToken={hideToken}
+          hidePrice={hidePrice}
+          onClick={handleClick}
+        />
+      );
+    };
 
-  return <Box>{isSingle ? renderSingle() : renderSelect()}</Box>;
-};
+    return <Box>{isSingle ? renderSingle() : renderSelect()}</Box>;
+  }
+);
 
 export default Field;
