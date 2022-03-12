176bf2e08cc94d3978f0fa20a57e4d589b0eab92
DW <dannywitters@gmail.com>
2022-03-12 14:42:14 +0000

    new api: governance proposal create msg apis

---
+++ modules/governance/api/createProposalMsg.ts
@@ -0,0 +1,30 @@
+import { toBase64 } from "@arthuryeti/terra";
+import { MsgExecuteContract } from "@terra-money/terra.js";
+import { GovernanceProposal } from "types/common";
+
+export const createProposalMsg = (
+  sender: string,
+  assembly: string,
+  xAstroToken: string,
+  amount: string,
+  proposal: GovernanceProposal
+) => {
+  const executeMsg = {
+    send: {
+      contract: assembly,
+      amount,
+      msg: toBase64({
+        submit_proposal: {
+          title: proposal?.title,
+          description: proposal?.description,
+          link: proposal?.link?.length > 0 ? proposal.link : null,
+          messages: proposal?.msg?.length > 0 ? proposal.msg : null,
+        },
+      }),
+    },
+  };
+
+  const msgs = new MsgExecuteContract(sender, xAstroToken, executeMsg);
+
+  return msgs;
+};
--- modules/governance/api/index.ts
+++ modules/governance/api/index.ts
@@ -1,1 +1,2 @@
 export * from "./createGovMsgs";
+export * from "./createProposalMsg";
