697d59c9db78dfba81227e0202ea196b9e63db04
wd021 <dannywitters@gmail.com>
2022-03-14 09:58:10 +0000

    fix: update xAstro balance after submit proposal [ASTRO-1508]

--- components/notifications/CreateProposalNotification.tsx
+++ components/notifications/CreateProposalNotification.tsx
@@ -1,4 +1,5 @@
 import React, { FC, useEffect } from "react";
+import { useQueryClient } from "react-query";
 import { TxInfo } from "@terra-money/terra.js";
 import { Text } from "@chakra-ui/react";
 
@@ -7,9 +8,14 @@ type Props = {
 };
 
 const CreateProposalNotification: FC<Props> = ({ txInfo }) => {
+  const queryClient = useQueryClient();
   const { logs } = txInfo;
   const { eventsByType } = logs[0];
   const proposalId = eventsByType.wasm.proposal_id[0];
+
+  useEffect(() => {
+    queryClient.invalidateQueries("balance");
+  }, []);
 
   return (
     <Text textStyle={["small", "medium"]}>
