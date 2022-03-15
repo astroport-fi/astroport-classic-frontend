3fe2946e3e6b8becec08d4da1f1daa6ad5b63611
wd021 <dannywitters@gmail.com>
2022-03-15 09:09:18 +0000

    fix: logic for auction contract rewards [ASTRO-1464]
    - initial claim + ongoing emissions programmed into useClaimAll
    - usePhase2Rewards hook only accounts for initial claim amount

--- modules/reward/hooks/useClaimAll.ts
+++ modules/reward/hooks/useClaimAll.ts
@@ -126,8 +126,12 @@ export const useClaimAll = ({ onBroadcasting, onError }: Params) => {
     }
 
     if (
-      auctionUserInfo != null &&
-      num(auctionUserInfo.auction_incentive_amount).gt(0)
+      // initial claim from phase 2
+      (auctionUserInfo != null &&
+        !auctionUserInfo.astro_incentive_transferred &&
+        num(auctionUserInfo.auction_incentive_amount).gt(0)) ||
+      // ongoing emissions from ust-astro in auction contract
+      num(auctionUserInfo.withdrawable_lp_shares).gt(0)
     ) {
       const phase2Msgs = createPhase2ClaimAllMsgs(
         {
