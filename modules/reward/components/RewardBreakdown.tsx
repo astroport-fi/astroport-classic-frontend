dbc07b8ce016366863e6d175ac2f025ad035e29b
Dani Roads <danicaminos@gmail.com>
2022-03-15 18:34:47 -0300

    feature: add new switch component

--- modules/reward/components/RewardBreakdown.tsx
+++ modules/reward/components/RewardBreakdown.tsx
@@ -1,19 +1,13 @@
 import React, { FC } from "react";
-import { Box, Text, Flex, VStack } from "@chakra-ui/react";
-import numeral from "numeral";
+import { Box, Text, Flex, Switch } from "@chakra-ui/react";
 
-import { useContracts } from "modules/common";
-import {
-  usePhase1Rewards,
-  usePhase2Rewards,
-  useTotalRewardValueInUst,
-  useBreakdownRewards,
-  useBreakdownRewardsInUst,
-} from "modules/reward";
+import { useBreakdownRewardsInUst } from "modules/reward";
 
 import RewardLineItem from "components/reward/RewardLineItem";
+import useLocalStorage from "hooks/useLocalStorage";
 
 const RewardBreakdown: FC = () => {
+  const [renderPrice, setRenderPrice] = useLocalStorage("renderPrice", true);
   const rewardsInUst = useBreakdownRewardsInUst();
 
   if (rewardsInUst.length === 0) {
@@ -22,10 +16,21 @@ const RewardBreakdown: FC = () => {
 
   return (
     <Box>
-      <Flex justify="space-between" align="flex-start">
+      <Flex justify="space-between" align="center">
         <Text textStyle="minibutton" fontSize="xs">
           Rewards BreakDown
         </Text>
+        <Flex justify="space-between" align="center">
+          <Text textStyle="small" variant="dimmed">
+            {renderPrice ? "With price" : "Without price"}
+          </Text>
+          <Switch
+            isChecked={renderPrice}
+            onChange={(e) => setRenderPrice(!renderPrice)}
+            ml="2"
+            height="22px"
+          />
+        </Flex>
       </Flex>
       <Box maxH="300" py="2" overflowY="auto">
         {rewardsInUst.map((reward) => (
