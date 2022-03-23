6e4d9a77356589964c7d472ea71c95358dab5101
Dani Roads <danicaminos@gmail.com>
2022-03-23 11:16:09 -0300

    ASTRO-1504: filter rewards with price and with no price

--- modules/reward/components/RewardBreakdown.tsx
+++ modules/reward/components/RewardBreakdown.tsx
@@ -7,33 +7,45 @@ import RewardLineItem from "components/reward/RewardLineItem";
 import useLocalStorage from "hooks/useLocalStorage";
 
 const RewardBreakdown: FC = () => {
-  const [renderPrice, setRenderPrice] = useLocalStorage("renderPrice", true);
+  const [renderRewardsWithPrice, setRenderRewardsWithPrice] = useLocalStorage(
+    "renderRewardsWithPrice",
+    true
+  );
   const rewardsInUst = useBreakdownRewardsInUst();
 
   if (rewardsInUst.length === 0) {
     return null;
   }
 
+  const renderSwitch = rewardsInUst.filter((r) => r.price === 0).length > 0;
+  const rewardsToShow =
+    renderSwitch && renderRewardsWithPrice === false
+      ? rewardsInUst.filter((r) => r.price === 0)
+      : rewardsInUst.filter((r) => r.price !== 0);
   return (
     <Box>
       <Flex justify="space-between" align="center">
         <Text textStyle="minibutton" fontSize="xs">
           Rewards BreakDown
         </Text>
-        <Flex justify="space-between" align="center">
-          <Text textStyle="small" variant="dimmed">
-            {renderPrice ? "With price" : "Without price"}
-          </Text>
-          <Switch
-            isChecked={renderPrice}
-            onChange={(e) => setRenderPrice(!renderPrice)}
-            ml="2"
-            height="22px"
-          />
-        </Flex>
+        {renderSwitch && (
+          <Flex justify="space-between" align="center">
+            <Text textStyle="small" variant="dimmed">
+              {renderRewardsWithPrice ? "With price" : "Without price"}
+            </Text>
+            <Switch
+              isChecked={renderRewardsWithPrice}
+              onChange={(e) =>
+                setRenderRewardsWithPrice(!renderRewardsWithPrice)
+              }
+              ml="2"
+              height="22px"
+            />
+          </Flex>
+        )}
       </Flex>
       <Box maxH="300" py="2" overflowY="auto">
-        {rewardsInUst.map((reward) => (
+        {rewardsToShow.map((reward) => (
           <RewardLineItem
             key={reward.token}
             token={reward.token}
