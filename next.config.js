f9dea6ba8a527b3f57e0d3bf39e7b03df5bfb617
Dani Roads <danicaminos@gmail.com>
2022-03-30 19:58:24 -0300

    ASTRO-1364: fix low severity vulnerabilities

--- next.config.js
+++ next.config.js
@@ -1,9 +1,47 @@
 /**
  * @type {import('next').NextConfig}
  */
+
+const contentSecurityPolicy = `
+ default-src 'self'; 
+ font-src 'self' data: fonts.gstatic.com;
+ img-src * 'self' data:;
+ style-src 'self' 'unsafe-inline' fonts.googleapis.com; 
+ script-src 'self' 'unsafe-eval';
+ connect-src * 'self' data:;
+ `;
 const nextConfig = {
   experimental: {
     esmExternals: false,
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)?", // Matches all pages
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "SAMEORIGIN",
+          },
+          {
+            key: "Content-Security-Policy",
+            value: contentSecurityPolicy.replace(/\s{2,}/g, " ").trim(),
+          },
+          {
+            key: "Referrer-Policy",
+            value: "origin-when-cross-origin",
+          },
+          {
+            key: "Pragma",
+            value: "no-cache",
+          },
+          {
+            key: "Expires",
+            value: new Date().toString(),
+          },
+        ],
+      },
+    ];
   },
 };
 
