eb2306d46c83710261bfde73ded2665528a1654a
wd021 <dannywitters@gmail.com>
2022-04-10 11:56:11 +0100

    fix: ongoing emissions amount [ASTRO-1655]
    - claimable_generator_astro, not withdrawable_lp_shares for calc ongoing emission amount
    - refactor if statements

--- modules/reward/hooks/usePhase2Rewards.ts
+++ modules/reward/hooks/usePhase2Rewards.ts
@@ -2,10 +2,13 @@ import { useMemo } from "react";
 import { num } from "@arthuryeti/terra";
 
 import { useUserInfo } from "modules/auction";
+import { useContracts, useTokenInfo } from "modules/common";
 import { ONE_TOKEN } from "constants/constants";
 
 export const usePhase2Rewards = () => {
+  const { astroToken } = useContracts();
   const userInfo = useUserInfo();
+  const { getDecimals } = useTokenInfo();
 
   return useMemo(() => {
     if (userInfo == null) {
@@ -15,18 +18,14 @@ export const usePhase2Rewards = () => {
       };
     }
 
-    const ongoingEmissions = num(userInfo.withdrawable_lp_shares).eq(0)
+    const ongoingEmissions = num(userInfo.claimable_generator_astro).eq(0)
       ? 0
-      : num(userInfo.withdrawable_lp_shares).div(ONE_TOKEN).dp(6).toNumber();
+      : +userInfo.claimable_generator_astro / 10 ** getDecimals(astroToken);
 
-    if (userInfo.astro_incentive_transferred) {
-      return {
-        oneTimeRewards: 0,
-        ongoingEmissions,
-      };
-    }
-
-    if (num(userInfo.auction_incentive_amount).eq(0)) {
+    if (
+      userInfo.astro_incentive_transferred ||
+      num(userInfo.auction_incentive_amount).eq(0)
+    ) {
       return {
         oneTimeRewards: 0,
         ongoingEmissions,
